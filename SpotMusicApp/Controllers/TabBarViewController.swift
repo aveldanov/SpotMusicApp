b9627e71799a0a4af5a9d20af032ffbb47bcdbb4
Anton Veldanov <anton.veldanov@gmail.com>
2022-09-12 20:09:56 -0700

    iOS

---
+++ SpotMusicApp/Controllers/TabBarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarViewController.swift
+//  SpotMusicApp
+//
+//  Created by Anton Veldanov on 9/12/22.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+
+    
+}
