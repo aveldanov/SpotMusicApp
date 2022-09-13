98bd958568e750a812ffd4794c8919005a0c4e1e
Anton Veldanov <aveldanov@linkedin.com>
2022-09-12 19:59:26 -0700

    first commit

---
+++ SpotMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotMusicApp
+//
+//  Created by Anton Veldanov on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
