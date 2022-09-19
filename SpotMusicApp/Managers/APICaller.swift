6a1458e335c06328272f794089213ef4af1abd26
Anton Veldanov <anton.veldanov@gmail.com>
2022-09-18 20:37:01 -0700

    iOS

--- SpotMusicApp/Managers/APICaller.swift
+++ SpotMusicApp/Managers/APICaller.swift
@@ -12,8 +12,35 @@ final class APICaller {
     static let shared = APICaller()
     private init() {}
 
-    public func getUserProfile(completion: @escaping (Result<UserProfile,Error>)->Void) {
+    struct Constants {
+        static let baseAPIURL = "https://api.spotify.com/v1"
+        
+    }
 
+    enum APIError: Error {
+        case failedToGetData
+    }
+
+    public func getUserProfile(completion: @escaping (Result<UserProfile,Error>)->Void) {
+        createRequest(
+            with: URL(string: ""),
+            type: .GET) { baseRequest in
+                URLSession.shared.dataTask(with: baseRequest) { data, response, error in
+                    guard let data = data, error == nil else {
+                        completion(.failure(APIError.failedToGetData))
+                        return
+                    }
+
+                    do {
+                        let result = try JSONSerialization.jsonObject(with: data, options: [.allowFragments])
+                        print(result)
+                    } catch {
+                        completion(.failure(error))
+                    }
+
+
+                }.resume()
+            }
     }
     // MARK: - Private
 
@@ -22,13 +49,17 @@ final class APICaller {
         case POST
     }
 
-    private func createRequest(with url: URL?,completion: @escaping (URLRequest)->Void) {
+    private func createRequest(with url: URL?,
+                               type: HTTPMethod,
+                               completion: @escaping (URLRequest)->Void) {
         AuthManager.shared.withValidToken { token in
             guard let apiURL = url else {
                 return
             }
             var request = URLRequest(url: apiURL)
             request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
             completion(request)
 
         }
