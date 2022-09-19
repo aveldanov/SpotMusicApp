27a1177c9f23da220de86be13f744f26d3e0e6de
Anton Veldanov <anton.veldanov@gmail.com>
2022-09-18 19:40:28 -0700

    iOS

--- SpotMusicApp/Managers/AuthManager.swift
+++ SpotMusicApp/Managers/AuthManager.swift
@@ -11,6 +11,8 @@ import Foundation
 final class AuthManager {
 
     static let shared = AuthManager()
+    private var refreshingToken = false
+
 
     struct Constants {
         static let  clientID = "906d20caba24486eb1a6e9014f87a5ef"
@@ -103,7 +105,32 @@ final class AuthManager {
         }.resume()
     }
 
+    public func withValidToken(completion: @escaping (String)->Void) {
+        guard !refreshingToken else {
+            return
+        }
+
+
+        if shouldRefreshToken {
+            // Refresh
+            refreshIfNeeded { [weak self] success in
+                if let token = self?.accessToken, success {
+                    completion(token)
+                }
+            }
+        } else if let token = accessToken {
+            completion(token)
+        }
+    }
+
     public func refreshIfNeeded(completion: @escaping (Bool)->Void) {
+        // check if we are already refreshing
+        guard !refreshingToken else {
+            return
+        }
+
+        // continue
+
         guard shouldRefreshToken else {
             completion(true) // token is still valid
             return
@@ -112,10 +139,12 @@ final class AuthManager {
             return
         }
 
-        // get token
+        // refresh token
         guard let url = URL(string: Constants.tokenAPIURL) else {
             return
         }
+
+        self.refreshingToken = true
 
         var components = URLComponents()
         components.queryItems = [URLQueryItem(name: "grant_type", value: "refresh_token"),
@@ -142,6 +171,7 @@ final class AuthManager {
 
 
         URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            self?.refreshingToken = false // stopped refreshing token
             guard let data = data, error == nil else {
                 completion(false)
                 return
